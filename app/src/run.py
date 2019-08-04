b5f7c18dc0d8c34f1988e3c0bacb12394929904b
Fumihiro_Maejima <testerwatson1101@gmail.com>
2019-08-04 23:37:38 +0900

    add migration env.

--- app/src/run.py
+++ app/src/run.py
@@ -1,12 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
+from app import app
 
-
-@app.route("/")
-def hello():
-    from datetime import datetime
-    return "hello, " + datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-
+#@app.route("/")
+#def hello():
+#    from datetime import datetime
+#    return "hello, " + datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
